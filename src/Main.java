0ed346f4b4b1c2c89949710a2a957a0afbe3c2bb
Ibrahim Torabek <torabek@gmail.com>
2020-10-05 22:36:49 -0400

    Project finished.    *Console application.    *Show the letters to be queried as stars    *Dislpay guessed letter, or display error messages.

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,34 @@ public class Main extends Application {
         Scanner s = new Scanner(System.in);
         char userInput = 'n';
 
+        do {
+            int guess = 0;
 
+            HangMan hangMan = new HangMan();
+
+            do{
+                System.out.print("(Guess) Enter a letter in word " + String.valueOf(hangMan.getUserWord()) + " > ");
+                userInput = s.nextLine().toCharArray()[0];
+
+                guess = hangMan.checkWord(userInput);
+
+                if (guess == -1) {
+                    System.out.println(userInput + " is already in the word");
+                    continue;
+
+                } else if (guess == 0) {
+                    System.out.println(userInput + " is not in the word.");
+                }
+            } while (hangMan.getStatus());
+
+            System.out.println();
+            System.out.println("The word is " + String.valueOf(hangMan.getSelectedWord()) + ". You missed " + hangMan.getMissed() + " times.");
+
+
+            System.out.print("Do you want to guess another word? Enter y to play again > ");
+            userInput = s.nextLine().toCharArray()[0];
+
+        } while (userInput == 'y');
 
         s.close();
         System.exit(0);
